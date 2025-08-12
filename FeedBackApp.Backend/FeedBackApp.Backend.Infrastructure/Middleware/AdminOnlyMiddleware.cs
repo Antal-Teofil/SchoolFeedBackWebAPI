1d32186a6d9e48603215705f94a3578f72b808f0
KisMiska <galicaervin@gmail.com>
2025-08-12 16:01:44 +0300

    middleware?

---
+++ FeedBackApp.Backend/FeedBackApp.Backend.Infrastructure/Middleware/AdminOnlyMiddleware.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using System.Net;
+
+namespace FeedBackApp.Backend.Infrastructure.Middleware
+{
+    public class AdminOnlyMiddleware : IFunctionsWorkerMiddleware
+    {
+        public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+            var functionName = context.FunctionDefinition.Name;
+
+            var key = Environment.GetEnvironmentVariable("ADMIN_KEY");
+            if (string.IsNullOrEmpty(key) || !IsAdmin(key))
+            {
+                var req = await context.GetHttpRequestDataAsync();
+                var res = req.CreateResponse(HttpStatusCode.Forbidden);
+                res.WriteString("ADMIN ONLY!");
+
+                context.GetInvocationResult().Value = res;
+                return;
+            }
+            await next(context);
+        }
+
+        //private bool IsAdmin(string key)
+        //{
+
+        //}
+    }
+}
