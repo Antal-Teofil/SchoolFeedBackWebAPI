a40f79248597ebfbacf2f1420b99152a7dd284da
Lorincz-Gabriella <lorinczgabriella32@gmail.com>
2025-08-13 13:55:13 +0300

    Login Frontend Working

--- Review.Frontend/src/api/ReviewApi.js
+++ Review.Frontend/src/api/ReviewApi.js
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:7277/api";
 const apiClient = axios.create({
     baseURL: API_URL,
     headers: {
-        "Content-Type": "application/Json",
+        "Content-Type": "application/json",
     },
 });
 
