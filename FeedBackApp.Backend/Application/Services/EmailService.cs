60979a23842b2c9c444427985450b4f21c6bc752
KisMiska <galicaervin@gmail.com>
2025-08-18 14:13:26 +0300

    Added bulk email sending
    # Conflicts:
    #	FeedBackApp.Backend/Application/Services/EmailService.cs
    #	FeedBackApp.Backend/Application/Services/Interfaces/IEmailService.cs

---
+++ FeedBackApp.Backend/Application/Services/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using Application.Services.Interfaces;
+using System.Net;
+using System.Net.Mail;
+
+namespace Application.Services;
+
+public class EmailService : IEmailService
+{
+    private readonly string _fromAddress;
+    private readonly string _fromName;
+    private readonly string _appPassword;
+    public EmailService()
+    {
+        _fromAddress = Environment.GetEnvironmentVariable("EMAIL_FROM_ADDRESS");
+        _fromName = Environment.GetEnvironmentVariable("EMAIL_FROM_NAME");
+        _appPassword = Environment.GetEnvironmentVariable("EMAIL_APP_PASSWORD");
+    }
+
+    public async Task SendEmailAsync(string toEmail, string toName, string subject, string body, string? attachmentPath = null)
+    {
+        var from = new MailAddress(_fromAddress, _fromName);
+        var to = new MailAddress(toEmail, toName);
+
+        using var smtp = new SmtpClient("smtp.gmail.com", 587)
+        {
+            Credentials = new NetworkCredential(_fromAddress, _appPassword),
+            EnableSsl = true
+        };
+
+        using var message = new MailMessage(from, to)
+        {
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true
+        };
+
+        if (!string.IsNullOrEmpty(attachmentPath))
+        {
+            var attachment = new Attachment(attachmentPath);
+            message.Attachments.Add(attachment);
+        }
+
+        await smtp.SendMailAsync(message);
+    }
+
+    public async Task SendBulkEmailAsync(IEnumerable<string> toEmails, string subject, string body, string? attachmentPath = null)
+    {
+        foreach (var email in toEmails)
+        {
+            await SendEmailAsync(email, "", subject, body, attachmentPath);
+        }
+    }
+
+}
---
+++ FeedBackApp.Backend/Application/Services/Interfaces/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Application.Services.Interfaces;
+public interface IEmailService
+{
+    Task SendEmailAsync(string toEmail, string toName, string subject, string body, string? attachmentPath = null);
+    Task SendBulkEmailAsync(IEnumerable<string> toEmails, string subject, string body, string? attachmentPath = null);
+}
